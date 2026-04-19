68d598cb195f3069133109ce314adb06685bbcfa
Timon Schneider <mail@timonschneider.de>
2026-04-19 12:04:50 +0200

    Add: binary-tree-right-side-view - submission-0

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-0.py
@@ -0,0 +1,52 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        """
+        Adjusted level order traversal
+
+        level order traversal
+        - queue to process nodes by level
+        - each level has one array per level
+
+        - here, the rightmost node is in view but we need 
+          to process the other nodes because they might have 
+          in-view children
+        """
+        view = []
+        if root is None: 
+            return view
+
+        q = deque([(root, 0)])
+        level = 0
+
+        last = None
+        while q:
+            print("before", list(q))
+            curr_node, curr_level = q.popleft()
+            
+            if curr_node.left: 
+                q.append((curr_node.left, curr_level + 1))
+            if curr_node.right: 
+                q.append((curr_node.right, curr_level + 1))
+
+            print("after", list(q))
+
+            if curr_level > level:
+                level += 1
+                view.append(last.val)
+
+            if not q: 
+                view.append(curr_node.val)
+                
+            print(view)
+            print()
+
+            last = curr_node
+
+        return view
